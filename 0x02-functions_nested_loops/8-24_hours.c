8361f4823b013e8eb40c6559bb9a42a9c1a7cb48
Thomas <thomasodhiambo800@gmail.com>
2022-09-13 06:45:30 -0700

    Jack Bauer

---
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ * jack_bauer - prints the minutes in a twelve hour interval
+ */
+
+void jack_bauer(void)
+{
+	int minutes;
+
+	int hours;
+
+	for (hours = 0; hours < 24; hours++)
+	{
+		for (minutes = 0; minutes < 60; minutes++)
+		{
+			_putchar((hours / 10) + '0');
+			_putchar((hours / 10) + '0');
+			_putchar(':');
+			_putchar((minutes / 10) + '0');
+			_putchar((minutes / 10) + '0');
+			_putchar('\n');
+		}
+	}
+}
