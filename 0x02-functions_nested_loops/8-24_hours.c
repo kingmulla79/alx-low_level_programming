e055234471eadb5970ce7d9598fadb279ba84941
Thomas <thomasodhiambo800@gmail.com>
2022-09-13 06:50:37 -0700

    Jack Bauer

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -15,10 +15,10 @@ void jack_bauer(void)
 		for (minutes = 0; minutes < 60; minutes++)
 		{
 			_putchar((hours / 10) + '0');
-			_putchar((hours / 10) + '0');
+			_putchar((hours % 10) + '0');
 			_putchar(':');
 			_putchar((minutes / 10) + '0');
-			_putchar((minutes / 10) + '0');
+			_putchar((minutes % 10) + '0');
 			_putchar('\n');
 		}
 	}
